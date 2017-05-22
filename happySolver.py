3d09ac291d9547a89f24a72fd209e4a88731783a
Cole <cole.brower@gmail.com>
2017-05-22 08:19:40 -0700

    Added a solver for the happy ending problem

--- happySolver.py
+++ happySolver.py
@@ -4,9 +4,10 @@ from helper_func import *
 import itertools
 import random
 import argparse
+import time
 import sys
 
-ACCURACY = 1000
+ACCURACY = 10
 
 def main():
 	global ACCURACY
@@ -19,14 +20,50 @@ def main():
 	flag = False
 	while not flag:
 		flag = testSides(args.numsides, numPoints)
-	print("G({}) = {}".format(args.numsides, numPoints))
+		numPoints += 1
+	print("G({}) = {}".format(args.numsides, numPoints - 1))
 
 def testSides(numSides, numPoints):
 	pts = []
-	#iterate through every point possible somehow
-		if collinearityTest(pts):
-			continue
-		flag, poly = findConvex(pts, numSides)
-		if not flag:
-			return False
+	c = 0
+	print("{}".format(numPoints))
+	for i in range(0, numPoints):
+		pts.append([])
+		pts[i].append(0)
+		pts[i].append(0)
+	while updateAllPoints(pts):
+		if not areAnyPointsSame(pts) and not collinearityTest(pts):
+			flag, poly = findConvex(pts, numSides)
+			if not flag:
+				printPoints(pts)
+				time.sleep(1)
+				return False
+		if c % 1000000 == 0:
+			print()
+			printPoints(pts)
+			print()
+		c += 1
 	return True
+
+def updatePoint(pt):
+	global ACCURACY
+	if pt[0] >= ACCURACY:
+		if pt[1] >= ACCURACY:
+			pt[0] = 0
+			pt[1] = 0
+			return False
+		else:
+			pt[0] = 0
+			pt[1] += 1
+	else:
+		pt[0] += 1
+	return True
+
+def updateAllPoints(pts):
+	for pt in pts:
+		if updatePoint(pt):
+			return True
+	return False
+
+if __name__ == "__main__":
+	main()
--- helper_func.py
+++ helper_func.py
@@ -9,6 +9,10 @@ def genLineSegs(pts):
 		lns.append((pts[i][0], pts[i][1], pts[(i + 1) % length][0], pts[(i + 1) % length][1]))
 	return lns
 
+def printPoints(pts):
+	for pt in pts:
+		print("({}, {})".format(pt[0], pt[1]))
+
 def doEndPointsIntersect(ln1, ln2):
 	if ln1[0] == ln2[0] and ln1[1] == ln2[1]:
 		return True
@@ -20,9 +24,53 @@ def doEndPointsIntersect(ln1, ln2):
 		return True
 	return False
 
+def areAnyPointsSame(pts):
+	for points in itertools.permutations(pts, 2):
+		if points[0][0] == points[1][0] and points[0][1] == points[1][1]:
+			return True
+	return False
+
+def IntersectsVerticalLine(ln1, ln2):
+	#tests to see which y value is greater
+	if ln1[1] > ln1[3]:
+		#test to see if the ys of ln2 is greater than ln1
+		if ln1[1] < ln2[1] and ln1[1] < ln2[3]:
+			return False
+		#tests to see if the ys of ln2 is less than ln1
+		elif ln1[3] > ln2[1] and ln1[3] > ln2[3]:
+			return False
+		#tests to see if the xs of ln2 are less than ln1
+		elif  ln1[0] > ln2[0] and ln1[0] > ln2[0]:
+			return False
+		#tests to see if the xs of ln2 are greater than ln1
+		elif ln1[0] < ln2[0] and ln1[0] < ln2[2]:
+			return False
+		else:
+			return True
+	else:
+		#test to see if the ys of ln2 is greater than ln1
+		if ln1[3] < ln2[1] and ln1[3] < ln2[3]:
+			return False
+		#tests to see if the ys of ln2 is less than ln1
+		elif ln1[1] > ln2[1] and ln1[1] > ln2[3]:
+			return False
+		#tests to see if the xs of ln2 are less than ln1
+		elif  ln1[0] > ln2[0] and ln1[0] > ln2[0]:
+			return False
+		#tests to see if the xs of ln2 are greater than ln1
+		elif ln1[0] < ln2[0] and ln1[0] < ln2[2]:
+			return False
+		else:
+			return True
+
 def hasIntersection(ln1, ln2):
 	if doEndPointsIntersect(ln1, ln2):
 		return False
+	elif ln1[2] == ln1[0]:
+		return IntersectsVerticalLine(ln1, ln2)
+	elif ln2[2] == ln2[0]:
+		return IntersectsVerticalLine(ln2, ln1)
+		
 	i1 = [min(ln1[0], ln1[2]), max(ln1[0], ln1[2])]
 	i2 = [min(ln2[0], ln2[2]), max(ln2[0], ln2[2])]
 	ia = [max(i1[0], i2[0]), min(i1[1], i2[1])]
@@ -71,9 +119,19 @@ def findConvex(pts, n):
 	return False, pts
 
 def areCollinear(p0, p1, p2):
-	s1 = (p1[1] - p0[1]) / (p1[0] - p0[0])
-	s2 = (p2[1] - p1[1]) / (p2[0] - p1[0])
-	return s1 == s2
+	if p0[0] == p1[0]:
+		if p0[0] == p2[0]:
+			return True
+	elif p1[0] == p2[0]:
+		if p1[0] == p0[0]:
+			return True
+	elif p0[0] == p2[0]:
+		if p0[0] == p1[0]:
+			return True
+	else:
+		s1 = (p1[1] - p0[1]) / (p1[0] - p0[0])
+		s2 = (p2[1] - p1[1]) / (p2[0] - p1[0])
+		return s1 == s2
 
 def collinearityTest(pts):
 	for points in itertools.combinations(pts, 3):
